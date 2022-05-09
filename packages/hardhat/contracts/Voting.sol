ce1245baffa28214d166691647e9dead4a6aca2d
lokithe5th <lourenslinde@gmail.com>
2022-05-09 20:49:16 +0200

    Added natspec

--- packages/hardhat/contracts/Voting.sol
+++ packages/hardhat/contracts/Voting.sol
@@ -4,7 +4,8 @@ pragma solidity >=0.8.0 <0.9.0;
 /// @title  Voting App
 /// @author @lourenslinde
 /// @notice Allows addresses which have a stake in Balancer pool to vote on proposed add/remove actions on GnosisSafe
-/// @dev    
+/// @dev    **NOT FOR PRODUCTION USE** 
+/// @dev    This contract is part of a technical challenge for Daoism Systems, it has not been audited.
 
 //Preferred method of making calls, but to allow arbitrary proposal execution
 interface IGnosis {
@@ -23,13 +24,13 @@ contract Voting {
     IERC20 public balancerPoolToken;
 
     struct Proposal {
-        address target;    //  name of proposal
-        uint256 action;    //  0 -> addSigner, 1 -> removeSigner, 2 -> arbitrary tx
-        uint256 voteCount;  //  number of votes (taking into account weighting)
-        uint256 value;
-        bytes proposal;
-        bool passed;      //  false -> not executed, true -> already executed
-        bool voteEnded;
+        address target;     // Address to be removed, added, or called
+        uint8 action;       // 0 -> addSigner, 1 -> removeSigner, 2 -> arbitrary tx
+        uint256 voteCount;  // Number of votes cast
+        uint256 value;      // Value to be paid, 0 for action == 0 || == 1
+        bytes proposal;     // Bytes representation of proposal, 0x30 if action == 0 || == 1
+        bool passed;        // false -> unsuccessful, true -> successfull and executed
+        bool voteEnded;     // Vote has ended. If voteEnded == true && passed == false, vote was unsuccessful
     }
 
     //  Mapping of index => address => weight
@@ -39,69 +40,54 @@ contract Voting {
 
     Proposal[] public proposals;
 
-    /// @notice Votes for a given proposal
-    /// @dev    Only stakers may vote, if passed a proposal will be executed
+    /// @notice Vote logic
+    /// @dev    Only stakers may vote, if passed a proposal will be executed, action type determines execution path
     /// @param  proposalIndex:uint256 The index of the proposal being voted on 
     /// @return bool true if passed, false if failed
-    function vote(uint256 proposalIndex, uint8 voteType) public onlyStakers(msg.sender) returns (bool) {
-        require(proposalIndex < proposals.length, "Invalid proposalIndex");
-        require(voteType < 2, "Invalid voteType");
-        require(votesByProposalIndexByStaker[proposalIndex][msg.sender]== 0, "Already voted");
+    function vote(
+        uint256 proposalIndex, 
+        uint8 voteType
+        ) public onlyStakers(msg.sender) returns (bool) {
+            require(proposalIndex < proposals.length, "Invalid proposalIndex");
+            require(voteType < 2, "Invalid voteType");
+            require(votesByProposalIndexByStaker[proposalIndex][msg.sender]== 0, "Already voted");
         
-        if (voteType == 0) {
-            votesByProposalIndexByStaker[proposalIndex][msg.sender] = balancerPoolToken.balanceOf(msg.sender);
-            votesForProposalByIndex[proposalIndex] = balancerPoolToken.balanceOf(msg.sender);
-            console.logUint(votesForProposalByIndex[proposalIndex]);
-        } else {
-            votesByProposalIndexByStaker[proposalIndex][msg.sender] = balancerPoolToken.balanceOf(msg.sender);
-            votesAgainstProposalByIndex[proposalIndex] = balancerPoolToken.balanceOf(msg.sender);
-            console.logUint(votesAgainstProposalByIndex[proposalIndex]);
+            if (voteType == 0) {
+                votesByProposalIndexByStaker[proposalIndex][msg.sender] = balancerPoolToken.balanceOf(msg.sender);
+                votesForProposalByIndex[proposalIndex] = balancerPoolToken.balanceOf(msg.sender);
+                console.logUint(votesForProposalByIndex[proposalIndex]);
+            } else {
+                votesByProposalIndexByStaker[proposalIndex][msg.sender] = balancerPoolToken.balanceOf(msg.sender);
+                votesAgainstProposalByIndex[proposalIndex] = balancerPoolToken.balanceOf(msg.sender);
+                console.logUint(votesAgainstProposalByIndex[proposalIndex]);
+            }
+
+            //  If total votes > 50% of stakers then execute the proposal 
+            if (votesForProposalByIndex[proposalIndex] > (balancerPoolToken.totalSupply()/2)) {
+                console.log("In Vote => Execution logic");
+                executeProposal(proposalIndex);
+                proposals[proposalIndex].passed = true;
+
+                return true;
+            } else if (votesAgainstProposalByIndex[proposalIndex] > (balancerPoolToken.totalSupply()/2)) {
+                proposals[proposalIndex].voteEnded = true;
+            }
+
+            return false;
         }
-        
-        //  If total votes > 50% of stakers then execute the proposal 
-        if (votesForProposalByIndex[proposalIndex] > (balancerPoolToken.totalSupply()/2)) {
-            console.log("In Vote => Execution logic");
-            executeProposal(proposalIndex, proposals[proposalIndex].value);
-            proposals[proposalIndex].passed = true;
-            
-            return true;
-        } else if (votesAgainstProposalByIndex[proposalIndex] > (balancerPoolToken.totalSupply()/2)) {
-            proposals[proposalIndex].voteEnded = true;
-        }
 
-        return false;
-    }
-
-    /* @notice Submits a proposal for adding an address to GnosisSafe
-    /// @dev 
-    /// @param  proposedSigner:address The address to be added to the GnosisSafe
-    function proposeAddSigner(address proposedSigner, uint256 threshold) public {
-        bytes memory proposalPacked = abi.encode("addOwnerWithThreshold(address, uint256),",proposedSigner,",",threshold);
-        proposals.push(Proposal({
-            nominee: proposedSigner,
-            action: 0,
-            voteCount: 0,
-            proposal: proposalPacked,
-            executed: false
-        }));
-
-    }
-
-    function proposeRemoveSigner(address proposedRemove, uint256 _threshold) public {
-        bytes memory proposalPacked = abi.encode("removeOwner(address, uint256),",proposedRemove,",",_threshold);
-        proposals.push(Proposal({
-            nominee: proposedRemove,
-            action: 1,
-            voteCount: 0,
-            proposal: proposalPacked,
-            executed: false
-        }));
-    } */
-
+    /// @notice Proposal submission
+    /// @dev    Creates a Proposal struct which is added to the proposals array
+    /// @param  target:address to be added, removed or called
+    /// @param  threshold:uint8, the new threshold for Gnosis Safe
+    /// @param  actionType:uint8, if == 0 {add target to safe}, if == 1 {remove target from safe}, else execute arbitrary proposal
+    /// @param  proposalValue:uint256, Value to be sent with call. Should be 0 for actionTypes 0 || 1
+    /// @param  proposalBytes:bytes, Encoded proposal for arbitrary call execution
+    /// @return uint256 The newly submitted proposal's index
     function submitProposal(
         address target, 
-        uint256 threshold, 
-        uint256 actionType, 
+        uint8 threshold, 
+        uint8 actionType, 
         uint256 proposalValue, 
         bytes memory proposalBytes
         ) public returns (uint256) {
@@ -126,21 +112,12 @@ contract Voting {
             }));
             console.log("Proposal submitted");
             return proposals.length - 1;
-    }
+        }
 
-    /*
-    function executeAddSigner(uint256 proposalIndex, uint256 _threshold) public {
-        IGnosis(safeAddress).addOwnerWithThreshold(proposals[proposalIndex].nominee, _threshold);
-    }
-
-    function executeRemoveSigner(uint256 proposalIndex, uint256 _threshold, address prevOwner) public {
-        IGnosis(safeAddress).removeOwner(prevOwner, proposals[proposalIndex].nominee, _threshold);
-    }
-    */
-
+    /// @notice Executes a proposal once vote > 50% of totalSupply
+    /// @param  proposalIndex:uint256, index of the proposal to be executed
     function executeProposal(
-        uint256 proposalIndex, 
-        uint256 value
+        uint256 proposalIndex
         ) 
         internal returns (bool) 
         {
@@ -156,28 +133,22 @@ contract Voting {
                 return true;
             } 
             
-            (bool success, bytes memory result) = proposals[proposalIndex].target.call{value: value}(proposals[proposalIndex].proposal);
+            (bool success, bytes memory result) = proposals[proposalIndex].target.call{value: proposals[proposalIndex].value}(proposals[proposalIndex].proposal);
             require(success, "execution failed");
             proposals[proposalIndex].voteEnded = true;
             console.log("Ending execProp internal fx");
             return true;
-    }
+        }
 
-    constructor(address _bpt, address _safeAddress) {
-        setBalancerPoolToken(_bpt);
-        safeAddress = _safeAddress;
-    }
+    //  Setup of the balancer pool token to be tracked and the Gnosis Safe to be controlled
+    constructor(
+        address _bpt, 
+        address _safeAddress) {
+            safeAddress = _safeAddress;
+            balancerPoolToken = IERC20(_bpt);
+        }
 
-    function setSafeAddress(address _safeAddress) public {
-        safeAddress = _safeAddress;
-        console.logAddress(safeAddress);
-    }
-
-    function setBalancerPoolToken(address _bpt) public {
-        balancerPoolToken = IERC20(_bpt);
-        console.logAddress(address(balancerPoolToken));
-    }
-
+    /// Modifier to only allow stakers (who are represented by holding balancer pool tokens) to call functions
     modifier onlyStakers(address _voter) {
         require(balancerPoolToken.balanceOf(_voter) > 0,"not a staker");
         _;
