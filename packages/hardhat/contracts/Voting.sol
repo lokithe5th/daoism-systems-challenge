4eb01479f19576110f6278ec467b648b06cef6d1
lokithe5th <lourensmba@gmail.com>
2022-05-23 12:00:14 +0200

    Small natspec tweaks, indexed events

--- packages/hardhat/contracts/Voting.sol
+++ packages/hardhat/contracts/Voting.sol
@@ -3,11 +3,11 @@ pragma solidity >=0.8.0 <0.9.0;
 
 /// @title  Voting App
 /// @author @lourenslinde
-/// @notice Allows addresses which have a stake in Balancer pool to vote on proposed add/remove actions on GnosisSafe
+/// @notice Allows addresses which hold Balancer Pool Tokens to vote on proposed add/remove actions on a custom GnosisSafe
 /// @dev    **NOT FOR PRODUCTION USE** 
 /// @dev    This contract is part of a technical challenge for Daoism Systems, it has not been audited.
 
-//Preferred method of making calls
+/// @dev    Using the interface is the preferred method of making calls to the Gnosis Implementation
 interface IGnosis {
     function getOwners() external;
     function addOwnerWithThreshold(address owner, uint256 _threshold) external;
@@ -18,11 +18,12 @@ interface IGnosis {
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Voting {
-    event newProposal(uint256 proposalIndex);
-    event voted(uint256 proposalIndex, address voter, uint256 votes, uint8 voteType);
-    event proposalPassed(uint256 proposalIndex);
-    event proposalFailed(uint256 proposalIndex);
-    event proposalExecuted(uint256 proposalIndex);
+
+    event newProposal(uint256 indexed proposalIndex);
+    event voted(uint256 indexed proposalIndex, address voter, uint256 indexed votes, uint8 indexed voteType);
+    event proposalPassed(uint256 indexed proposalIndex);
+    event proposalFailed(uint256 indexed proposalIndex);
+    event proposalExecuted(uint256 indexed proposalIndex);
     event addSigner(address newSigner);
     event removeSigner(address removedSigner);
 
@@ -39,7 +40,7 @@ contract Voting {
         bool voteEnded;     // Vote has ended. If voteEnded == true && passed == false, vote was unsuccessful
     }
 
-    //  Mapping of index => address => weight
+    /// Mapping of index => address => weight
     mapping(uint256 => mapping(address => uint256)) private votesByProposalIndexByStaker;
     mapping(uint256 => uint256) private votesForProposalByIndex;
     mapping(uint256 => uint256) private votesAgainstProposalByIndex;
@@ -47,7 +48,7 @@ contract Voting {
     /// Array of Proposal structs
     Proposal[] public proposals;
 
-    //  Setup of the balancer pool token to be tracked and the Gnosis Safe to be controlled
+    /// Setup of the balancer pool token to be tracked and the Gnosis Safe to be controlled
     constructor(
         address _bpt, 
         address _safeAddress) {
@@ -59,7 +60,7 @@ contract Voting {
     /// @dev    Creates a Proposal struct which is added to the proposals array
     /// @param  target:address to be added, removed or called
     /// @param  threshold:uint8, the new threshold for Gnosis Safe
-    /// @param  actionType:uint8, if == 0 {add target to safe}, if == 1 {remove target from safe}, else execute arbitrary proposal
+    /// @param  actionType:uint8, if == 0 {add target to safe}, if == 1 {remove target from safe}, later can allow arbitrary proposal execution or other set function calls
     /// @param  proposalValue:uint256, Value to be sent with call. Should be the new threshold for actionTypes 0 || 1
     /// @param  proposal:string, description of proposal
     /// @return uint256 The newly submitted proposal's index
@@ -70,7 +71,7 @@ contract Voting {
         uint256 proposalValue, 
         string memory proposal
         ) public returns (uint256) {
-            require(actionType < 3, "Invalid action");
+            require(actionType < 2, "Invalid action");
 
             uint256 _value;
             if (actionType == 0 || actionType == 1) {
@@ -96,6 +97,7 @@ contract Voting {
     /// @notice Vote logic
     /// @dev    Only stakers may vote, if passed a proposal will be executed, action type determines execution path
     /// @param  proposalIndex:uint256 The index of the proposal being voted on 
+    /// @param  voteType:uint8 If "0" -> vote for proposal, if "1" vote against proposal
     /// @return bool true if passed, false if failed
     function vote(
         uint256 proposalIndex, 
@@ -104,7 +106,7 @@ contract Voting {
             require(proposalIndex < proposals.length, "Invalid proposalIndex");
             require(voteType < 2, "Invalid voteType");
             require(votesByProposalIndexByStaker[proposalIndex][msg.sender]== 0, "Already voted");
-            require(!proposals[proposalIndex].voteEnded,"Vote has ended");
+            require(!proposals[proposalIndex].voteEnded, "Vote has ended");
         
             //  Voting logic
             countVotes(proposalIndex, voteType);
@@ -180,7 +182,7 @@ contract Voting {
 
     /// Modifier to only allow stakers (who are represented by holding balancer pool tokens) to call functions
     modifier onlyStakers(address _voter) {
-        require(balancerPoolToken.balanceOf(_voter) > 0,"not a staker");
+        require(balancerPoolToken.balanceOf(_voter) > 0, "not a staker");
         _;
     }
 
